9ee9e1c05e2c44f9c87958277934cab8beb77f64
paradox8599 <paradox8599@outlook.com>
2025-05-07 14:56:00 +1000

    chore: remove GitHub config files and restrict extension to VRChat

--- chrome-extension/manifest.ts
+++ chrome-extension/manifest.ts
@@ -46,15 +46,15 @@ const manifest = {
   },
   content_scripts: [
     {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+      matches: ['https://vrchat.com/home/*'],
       js: ['content/index.iife.js'],
     },
     {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+      matches: ['https://vrchat.com/home/*'],
       js: ['content-ui/index.iife.js'],
     },
     {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+      matches: ['https://vrchat.com/home/*'],
       css: ['content.css'],
     },
   ],
